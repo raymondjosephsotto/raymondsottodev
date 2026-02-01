18ece1724e011fda91a85738a70bbec249f5ec66
sinagdev <ray.sinag.dev@gmail.com>
2026-02-01 11:52:00 -0700

    Update NavBar layout for improved button alignment and responsiveness

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,7 +6,13 @@ import NavButton from "./NavButton";
 const NavBar: React.FC = () => {
     return (
         <AppBar position="sticky" style={{ backgroundColor: '#333' }}>
-            <Toolbar style={{ justifyContent: 'center' }}>
+            <Toolbar 
+                sx={{ 
+                    justifyContent: 'space-between', 
+                    margin: '0 auto',
+                    width: { xs: '75%', md: '50%' }
+                }}
+            >
                 <NavButton color="inherit" label="Home" icon={<Home />} to="/" end />
                 <NavButton color="inherit" label="About" icon={<Person />} to="/about" />
                 <NavButton color="inherit" label="Projects" icon={<Work />} to="/projects" />
